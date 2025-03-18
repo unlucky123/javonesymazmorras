0d78a2171a96b1d7e1b79c334ccd5075d27a55eb
kkkarolina12 <kliminakaro@alumnes.ilerna.com>
2025-03-18 19:45:32 +0100

    Update Inventario.java

--- Inventario.java
+++ Inventario.java
@@ -1,9 +1,24 @@
 package dragonesymazmorras;
 
+import java.util.ArrayList;
+
 public class Inventario {
+	 
+	    private ArrayList<Equipamiento> equipamiento;
 
-	public static void main(String[] args) {
+	    public Inventario() {
+	        this.equipamiento = new ArrayList<>();
+	    }
 
+	    public void agregarEquipamiento(Equipamiento equipamiento) {
+	        this.equipamiento.add(equipamiento);
+	    }
+
+	    public void mostrarInventario() {
+	        System.out.println("Inventario:");
+	        for (Equipamiento e : equipamiento) {
+	            System.out.println(e.getNombre() + " - " + e.getTipo());
+	        }
 	}
 
 }
