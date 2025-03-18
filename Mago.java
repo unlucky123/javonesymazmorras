258bdd487686055d09681fb01e8012dbb76414de
unlucky123 <66431033+unlucky123@users.noreply.github.com>
2025-03-18 20:05:48 +0100

    Add files via upload

---
+++ Mago.java
@@ -0,0 +1,15 @@
+package dragonesymazmorras;
+
+public class Mago extends Personaje{
+
+	public Mago(String nombre, int nivel, int puntosVida, Inventario inventario) {
+		super(nombre, nivel, puntosVida, inventario);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	
+
+
+
+}
