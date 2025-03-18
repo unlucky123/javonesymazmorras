847f68bef46d6e1471fd118171023e1a799689b5
unlucky123 <66431033+unlucky123@users.noreply.github.com>
2025-03-18 20:06:15 +0100

    Update Mago.java

--- Mago.java
+++ Mago.java
@@ -1,10 +1,20 @@
 package dragonesymazmorras;
 
-public class Mago extends Personaje{
+public class Mago extends Personaje {
 
 	public Mago(String nombre, int nivel, int puntosVida, Inventario inventario) {
 		super(nombre, nivel, puntosVida, inventario);
-		// TODO Auto-generated constructor stub
+
+	}
+
+	public void lanzarHechizo(int cosotoMana) {
+
+	}
+
+	public void recargarMana(int cantidad) {
+	}
+	public void getMana() {
+		
 	}
 
 
