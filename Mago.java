77050322c0b72f34851b3982f93be4a8a78e2958
unlucky123 <66431033+unlucky123@users.noreply.github.com>
2025-03-19 14:23:39 +0100

    Update Mago.java
    clase acabada?

--- Mago.java
+++ Mago.java
@@ -2,17 +2,30 @@ package dragonesymazmorras;
 
 public class Mago extends Personaje {
 	private int mana;
-	public Mago(String nombre, int nivel, int puntosVida, Inventario inventario) {
+	private int costoMana;
+	public Mago(String nombre, int nivel, int puntosVida, Inventario inventario, int mana, int costoMana) {
 		super(nombre, nivel, puntosVida, inventario);
 		this.mana = mana;
+		this.costoMana = costoMana;
 	}
-	//metodo par alanzar un hechizo
+	//metodo para lanzar un hechizo
 	public void lanzarHechizo(int cosotoMana) {
-
+		if (mana >=costoMana){
+			mana-=costoMana;
+			System.out.println(nombre + "ha usado un hechizo")
+		}
+		else{
+			System.out.println("Te falta Mana!!!!")
+		}
 	}
 	//metodo para recargar el mana
 	public void recargarMana(int cantidad) {
-		
+		if (cantidad > 0) {
+            mana += cantidad;
+            System.out.println(nombre + " recarga " + cantidad + " puntos de mana.");
+        } else {
+            System.out.println("La cantidad de recarga debe ser positiva.");
+        }
 	}
 	//metodo para que el mana no pueda estar en numeros negativos
 	public void setMana(int mana){
