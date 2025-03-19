101ca723168ed3fab82a7660116fa2686c343450
unlucky123 <66431033+unlucky123@users.noreply.github.com>
2025-03-19 12:31:59 +0100

    Update Mago.java
    afegir mana

--- Mago.java
+++ Mago.java
@@ -1,20 +1,31 @@
 package dragonesymazmorras;
 
 public class Mago extends Personaje {
-
+	private int mana;
 	public Mago(String nombre, int nivel, int puntosVida, Inventario inventario) {
 		super(nombre, nivel, puntosVida, inventario);
-
+		this.mana = mana;
 	}
-
+	//metodo par alanzar un hechizo
 	public void lanzarHechizo(int cosotoMana) {
 
 	}
-
+	//metodo para recargar el mana
 	public void recargarMana(int cantidad) {
-	}
-	public void getMana() {
 		
+	}
+	//metodo para que el mana no pueda estar en numeros negativos
+	public void setMana(int mana){
+		if(mana >=0){
+			this.mana = mana;
+		}
+		else{
+			system.out.println("El mana no puede estar en numeros negativos")
+		}
+	}
+	//metodo para ver el mana que tiene
+	public int getMana() {
+		return mana;
 	}
 
 
