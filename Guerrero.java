92cb3f4a5e14577f2f3a69ed2b81169cceebbe87
unlucky123 <66431033+unlucky123@users.noreply.github.com>
2025-03-19 12:32:21 +0100

    Update Guerrero.java
    afegir "Protejer"

--- Guerrero.java
+++ Guerrero.java
@@ -1,5 +1,9 @@
 package dragonesymazmorras;
 
-public class Guerrero {
+public class Guerrero extends Personaje {
+	private boolean Protejer;
+  public Guerrero(String nombre, int nivel, int puntosVida, Inventario inventario) {
+		super(nombre, nivel, puntosVida, inventario);
 
-}
+	}
+
